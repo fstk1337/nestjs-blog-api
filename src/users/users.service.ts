cd1721ffee98b419d03c06f7a23f0d742f9e71ee
Vitali Shvaichuk <vit.shvaichuk@gmail.com>
2025-02-14 23:54:54 +0300

    minor: remove passwordHash from API responses

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -8,26 +8,36 @@ export class UsersService {
   constructor(private readonly prisma: DatabaseService) {}
 
   create(createUserDto: CreateUserDto) {
-    return this.prisma.user.create({ data: createUserDto });
+    return this.prisma.user.create({
+      data: createUserDto,
+      omit: { passwordHash: true },
+    });
   }
 
   findAll() {
-    return this.prisma.user.findMany();
+    return this.prisma.user.findMany({ omit: { passwordHash: true } });
   }
 
   findOne(id: number) {
-    return this.prisma.user.findUnique({ where: { id } });
+    return this.prisma.user.findUnique({
+      where: { id },
+      omit: { passwordHash: true },
+    });
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
     return this.prisma.user.update({
       where: { id },
       data: updateUserDto,
+      omit: { passwordHash: true },
     });
   }
 
   remove(id: number) {
-    return this.prisma.user.delete({ where: { id } });
+    return this.prisma.user.delete({
+      where: { id },
+      omit: { passwordHash: true },
+    });
   }
 
   async userExists(id: number) {
